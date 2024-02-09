fbf0c8d4d94859bb91422f47c3bf0236e3357173
lakshyeahh <verma.lakshya071@gmail.com>
2024-02-10 01:43:25 +0530

    Environment

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pg = require("pg");
+
+
+
+//Routes
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "NewDB",
+    password: "lakshya",
+    port: 5432,
+});
+
+db.connect();
+
+app.use(cors());
+app.use(express.json()); //req.body
+
+
+
+app.listen(3001, () => {
+    console.log("server has started on port 3001");
+  });
+
