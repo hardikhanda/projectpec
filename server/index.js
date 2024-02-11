ae7008686b8852efa94068ad369e146c51a25e9f
lakshyeahh <verma.lakshya071@gmail.com>
2024-02-12 00:42:58 +0530

    connect

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { createClient } = require('@supabase/supabase-js')
 const app = express();
+const path = require('path'); // Import the path module
 
 app.use(cors());
 app.use(express.json()); //req.body
@@ -11,21 +12,13 @@ const supabase = createClient(
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVicndkZXFtdW9ldmVoanZwcmliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc1NjQ5MTMsImV4cCI6MjAyMzE0MDkxM30.ffPJDAhL5f1h8QzK27oz7h71tUI2yEgFowdt3yOpPmU'
 )
 
-const main = async () => {
-  let { data, error } = await supabase
-    .from('user')
-    .select('*')
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-  if (error) {
-    console.error(error)
-    return
-  }
-
-  console.log(data)
-}
-
-main()
-app.listen(6000, () => {
+app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
